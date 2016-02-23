007f4652fa6793b7caaeb7558076dbdf81d3e251
Justin Pease <justinpease2@gmail.com>
2016-02-23 13:08:59 -0700

    finishes the routes for find for all 6 categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,22 @@ Rails.application.routes.draw do
         get 'find', to: 'find#show'
       end
 
+      namespace :items do
+        get 'find', to: 'find#show'
+      end
+
+      namespace :invoices do
+        get 'find', to: 'find#show'
+      end
+
+      namespace :invoice_items do
+        get 'find', to: 'find#show'
+      end
+
+      namespace :transactions do
+        get 'find', to: 'find#show'
+      end
+
       resources :items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
