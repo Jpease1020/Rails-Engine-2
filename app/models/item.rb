2e5421bff32e829728e005b5e625650b50c77d8a
Justin Pease <justinpease2@gmail.com>
2016-02-24 16:01:22 -0700

    adds items most revenue method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
   has_many :invoice_items
+  has_many :invoices, through: :invoice_items
   belongs_to :merchant
+
+  def self.most_revenue(count)
+    Item.select("items.*, count('invoice_item.unit_price * invoice_item.quantity') AS item_revenue").joins(invoice_items: [invoice: :transactions]).group("items.id").where("transactions.result = ?", "success").order("item_revenue DESC").take(count)
+  end
 end
