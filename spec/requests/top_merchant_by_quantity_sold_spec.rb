3cc6a6472a68fb2a39e34ca6604e6902a152804a
Justin Pease <justinpease2@gmail.com>
2016-02-25 03:57:53 -0700

    adds test for top merchant by items sold

--- spec/requests/top_merchant_by_quantity_sold_spec.rb
+++ spec/requests/top_merchant_by_quantity_sold_spec.rb
@@ -1,28 +1,31 @@
-# require 'rails_helper'
-#
-# describe "item" do
-#   describe "GET /api/v1/items/most_items?quantity=x" do
-#     it "returns the top x item instances ranked by total number sold" do
-#       item_list_1            = create_list(:item, 7)
-#       item_list_2            = create_list(:item, 5)
-#       invoice_1         = create(:invoice)
-#       transaction_1     = create(:transaction, invoice_id: invoice_1.id, result: "success")
-#       invoice_items_1   = create_list(:invoice_item, 5, item_id: item_list_1.first.id, invoice_id: invoice_1.id)
-#       invoice_items_2   = create_list(:invoice_item, 3, item_id: item_list_2.first.id, invoice_id: invoice_1.id)
-#       invoice_items_3   = create_list(:invoice_item, 3, item_id: item_list_1.second.id, invoice_id: invoice_1.id)
-#
-#       get "/api/v1/items/most_items?quantity=3", {}, { "Accept" => "application/json" }
-#
-#       expect(response.status).to eq 200
-#
-#       items = JSON.parse(response.body)
-#
-#       assert_equal 3, items.count
-#
-#       assert_equal items.last['id'], 97
-#
-#       assert items.first['item_count'] >= items.second['item_count']
-#       assert items.second['item_count'] >= items.third['item_count']
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe "item" do
+  describe "GET /api/v1/merchants/most_items?quantity=x" do
+    it "returns the top x merchants ranked by total number of items sold" do
+      item_list_1            = create_list(:item, 7)
+      merchant_1             = create(:merchant)
+      merchant_2             = create(:merchant)
+      merchant_3             = create(:merchant)
+      invoice_1              = create(:invoice, merchant_id: merchant_1.id)
+      invoice_2              = create(:invoice, merchant_id: merchant_2.id)
+      invoice_3              = create(:invoice, merchant_id: merchant_3.id)
+      transaction_1     = create(:transaction, invoice_id: invoice_1.id, result: "success")
+      transaction_2     = create(:transaction, invoice_id: invoice_2.id, result: "success")
+      transaction_3     = create(:transaction, invoice_id: invoice_3.id, result: "success")
+      invoice_items_1   = create_list(:invoice_item, 15, item_id: item_list_1.first.id, invoice_id: invoice_1.id)
+      invoice_items_2   = create_list(:invoice_item, 3, item_id: item_list_1.third.id, invoice_id: invoice_2.id)
+      invoice_items_3   = create_list(:invoice_item, 3, item_id: item_list_1.second.id, invoice_id: invoice_3.id)
+
+      get "/api/v1/merchants/most_items?quantity=3", {}, { "Accept" => "application/json" }
+
+      expect(response.status).to eq 200
+
+      merchants = JSON.parse(response.body)
+      assert_equal 3, merchants.count
+
+      assert merchants.first['total_sold'].to_i >= merchants.second['total_sold'].to_i
+      assert merchants.second['total_sold'].to_i >= merchants.third['total_sold'].to_i
+    end
+  end
+end
