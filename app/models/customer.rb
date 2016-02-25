27173b55919120ae22f28d4f93b82f948e764595
Justin Pease <justinpease2@gmail.com>
2016-02-25 01:00:36 -0700

    adds favorite merchant method to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def self.favorite_merchant(id)
+    select("merchants.*").joins(invoices: [:merchant, :transactions]).where('transactions.result = ? AND customers.id = ?', "success", id).group('merchants.id').order('count(invoices.merchant_id) DESC').first
+  end
 end
