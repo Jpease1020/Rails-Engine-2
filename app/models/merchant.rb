8dd3fa566efa91b44e38ce483574d8acdaacb58d
Justin Pease <justinpease2@gmail.com>
2016-02-25 00:41:07 -0700

    adds favorite customer method to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -21,7 +21,7 @@ class Merchant < ActiveRecord::Base
   end
 
   def self.revenue(id)
-    {"revenue"=> joins(invoices: [:invoice_items, :transactions])
+    { "revenue"=> joins(invoices: [:invoice_items, :transactions])
                   .where('transactions.result = ? AND merchants.id = ?', "success", id)
                   .sum("invoice_items.quantity * invoice_items.unit_price")}
   end
@@ -43,5 +43,29 @@ class Merchant < ActiveRecord::Base
     { "total_revenue" => joins(invoices: [:invoice_items, :transactions])
                           .where('invoices.updated_at = ? AND transactions.result = ?', date, "success")
                           .sum("invoice_items.quantity * invoice_items.unit_price")}
+  end
+
+  def self.favorite_customer(id)
+            # first try
+    # hash = select("customers.*").joins(invoices: [:customer, :transactions]).group('customer_id').where('transactions.result = ? AND merchants.id = ?', "success", id).count
+    # max_value = select("customers.*").joins(invoices: [:customer, :transactions]).group('customer_id').where('transactions.result = ? AND merchants.id = ?', "success", id).count.values.max
+    # cus_id = hash.key(max_value)
+    # Customer.find(cus_id)
+
+            # looking at the answer
+    # Customer.joins(invoices: :transactions)
+    #             .where("invoices.merchant_id = ? AND transactions.result = 'success'", id)
+    #             .group('id')
+    #             .order('count(invoices.customer_id) DESC')
+    #             .first
+
+
+              # refactored
+    select("customers.*")
+      .joins(invoices: [:customer, :transactions])
+      .where('transactions.result = ? AND merchants.id = ?', "success", id)
+      .group('customers.id')
+      .order('count(invoices.customer_id) DESC')
+      .first
   end
 end
